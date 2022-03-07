377943cef6632b46fbdd6bc22036fb8016594c63
Anseroidea <K1525058@students.katyisd.rog>
2022-03-07 15:37:43 -0600

    changes

--- src/main/java/HexPaneApplication.java
+++ src/main/java/HexPaneApplication.java
@@ -1,9 +1,12 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
@@ -16,11 +19,21 @@ public class HexPaneApplication extends Application {
         HexGridPane<Tile> hexGridPane = new HexGridPane<>(42);
         BufferedImage i = ImageIO.read(new File("C:\\Users\\slhscs216\\Downloads\\testbar.png"));
         ImageView im = new ImageView(SwingFXUtils.toFXImage(i, null));
-        im.setRotate(90);
-        primaryStage.setScene(new Scene(new AnchorPane(im)));
         double height = i.getHeight();
         System.out.println(height);
         double width = i.getWidth();
+        AnchorPane ap = new AnchorPane(new StackPane(im));
+        VBox vb = new VBox(ap);
+        Slider sl = new Slider();
+        sl.setMin(0);
+        sl.setMax(360);
+        sl.valueProperty().addListener((observable, oldValue, newValue) -> {
+            im.setRotate(newValue.doubleValue());
+            
+        });
+        vb.getChildren().add(sl);
+        primaryStage.setScene(new Scene(new StackPane(vb)));
+
 
         im.setLayoutY(width/2 - height/2);
         im.setLayoutX(-height/2);
